5e5a9311c33a7607761d689b3e24308c83357edd
mydavids <yusiry.davids@gmail.com>
2016-10-18 08:00:46 +0200

    Position Factories and Tests

---
+++ test/factories/position/PositionPackageTest.scala
@@ -0,0 +1,33 @@
+package factories.position
+
+import domain.position.PositionPackage
+import org.joda.time.DateTime
+import org.scalatest.FunSuite
+
+/**
+  * Created by Yusiry on 10/17/2016.
+  */
+class PositionPackageTest extends FunSuite {
+
+  test("testCreatePositionFunding") {
+    val des = new PositionPackageFactory;
+
+    val date = new DateTime(2016, 4, 2, 1, 20, 2, 0);
+
+    val values = Map("positionId" -> "1",
+      "positionPackageId" -> "1",
+      "gradeId" -> "1",
+      "notchId" -> "1",
+      "state" -> "testState"
+    );
+
+    val posdes = des.createPositionPackageFactory(values, date);
+
+    assert(posdes == PositionPackage(positionId = "1",
+      positionPackageId = "1",
+      gradeId = "1",
+      notchId = "1",
+      state = "testState",
+      date = new DateTime(2016, 4, 2, 1, 20, 2, 0)));
+  }
+}
