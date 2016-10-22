82b40fe6ec6f475000e6e9a6ea6d6a015c0d486c
louise mukendi <louisane006@gmail.com>
2016-10-22 23:40:41 +0200

    commit

--- app/factories/job/JobEventFactory.scala
+++ app/factories/job/JobEventFactory.scala
@@ -1,4 +1,5 @@
 package factories.job
+
 import domain.job.JobEvent
 import org.joda.time.DateTime
 
@@ -6,7 +7,8 @@ import org.joda.time.DateTime
   * Created by hashcode on 2016/09/26.
   */
 class JobEventFactory {
-  //def createJobEvent(values:Map[String, String],date:DateTime):JobEvent={
-  //JobEvent(jobId = values("jobId"),jobEventId = values("jobEventId"),date=date,event = values("event"))
-//}
+  def createJobEvent(values:Map[String, String],date:DateTime):JobEvent={
+    JobEvent(jobId = values("jobId"),jobEventId = values("jobEventId"),date=date,event = values("event"))
+  }
+
 }
--- app/factories/payroll/funding/FundingSourcesFactory.scala
+++ app/factories/payroll/funding/FundingSourcesFactory.scala
@@ -1,12 +1,16 @@
+
 package factories.payroll.funding
+
 import domain.payroll.funding.FundingSources
+import org.joda.time.DateTime
 
 /**
   * Created by Malu.Mukendi on 2016-09-30.
   */
-object FundingSourcesFactory {
-  def createFundingSources(values:Map[String, String]):FundingSources={
-    FundingSources(organisationId = values("organisationId"),fundingSourcesId = values("fundingSourcesId"),
-      name = values("name"),costCenterNumber = values("costCenterNumber"),  date = values("date"),details = values)
-  }
+object FundingSourcesFactory
+{
+  def createFundingSources(values: Map[String, String], date: DateTime, details: Map[String, String]): FundingSources=
+    FundingSources(organisationId = values("organisationId"), fundingSourcesId = values("fundingSourcesId"),  name = values("name"),
+      costCenterNumber = values("costCenterNumber"), date = date, details = details)
+
 }
