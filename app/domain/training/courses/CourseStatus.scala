91b8a2b5b059b64a8fdd2aecadf65748bbba2b19
gavin joel ackerman <gavin.j.ackerman@gmail.com>
2016-11-21 09:36:11 +0200

    no message

--- app/domain/training/courses/CourseStatus.scala
+++ app/domain/training/courses/CourseStatus.scala
@@ -7,7 +7,7 @@ import play.api.libs.json.Json
 /**
   * Created by hashcode on 2016/10/17.
   */ 
-class CourseStatus(courseId: String, status: String, date: DateTime)
+class CourseStatus(courseId: String, status: String, date: Date)
 
 object CourseStatus {
   implicit val courseStatus = Json.format[CourseStatus]
