059f9ea55ab03c70e30bf142b2431c6354e37619
gavin joel ackerman <gavin.j.ackerman@gmail.com>
2016-11-24 13:02:43 +0200

    no message

--- app/factories/training/competency/CompetencyFactory.scala
+++ app/factories/training/competency/CompetencyFactory.scala
@@ -10,7 +10,7 @@ import domain.training.competencies.Competency
  * Created by gavin.ackerman on 2016-10-23.
  */
  object CompetencyFactory {
-  def createCompetency(values:util.Map[String, String]):Competency={
+  def createCompetency(values:Map[String, String]):Competency={
     Competency(compencyId = values("compencyId"), name = values("name"), competencyTypeId = values("competencyTypeId"))
   }
 }
