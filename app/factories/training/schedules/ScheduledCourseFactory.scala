3ee00ecd005d6df9963e8ab2bac78ef989a29b5a
gavin joel ackerman <gavin.j.ackerman@gmail.com>
2016-11-25 10:22:35 +0200

    no message

--- app/factories/training/schedules/ScheduledCourseFactory.scala
+++ app/factories/training/schedules/ScheduledCourseFactory.scala
@@ -1,6 +1,6 @@
 package factories.training.schedules
 
-import java.util.Date
+import org.joda.time.DateTime
 
 import domain.training.schedules.ScheduledCourse
 
@@ -8,7 +8,7 @@ import domain.training.schedules.ScheduledCourse
  * Created by gavin.ackerman on 2016-10-23.
  */
 object ScheduledCourseFactory {
-  def createScheduledCourse(values:Map[String, String],capacity:Int,hours:Int,start:Date,end:Date,sched:Date):ScheduledCourse={
+  def createScheduledCourse(values:Map[String, String],capacity:Int,hours:Int,start:DateTime,end:DateTime,sched:DateTime):ScheduledCourse={
     ScheduledCourse(organisationId = values("organisationId"), courseId = values("courseId"),scheduledCourseId = values("scheduledCourseId"),
       venue = values("venue"), courseCapacity =capacity,creditHours = hours,
       startDate = start, endDate = end,locationId = values("locationId"),dateScheduled = sched)
