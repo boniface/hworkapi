d47613be788210858fac1f161307f05434f0ac40
gavin joel ackerman <gavin.j.ackerman@gmail.com>
2016-11-25 10:42:34 +0200

    no message

--- app/factories/training/courses/CourseStatusFactory.scala
+++ app/factories/training/courses/CourseStatusFactory.scala
@@ -1,15 +1,15 @@
 package factories.training.courses
 
-import java.util
-import java.util.Date
+
 
 import domain.training.courses.CourseStatus
+import org.joda.time.DateTime
 
 /**
  * Created by gavin.ackerman on 2016-10-23.
  */
 object CourseStatusFactory {
-  def createCourseStatus(values:util.Map[String, String],dates:Date):CourseStatus={
+  def createCourseStatus(values:Map[String, String],dates:DateTime):CourseStatus={
     CourseStatus(courseId = values("courseId"), status = values("status"),date = dates)
   }
 }
